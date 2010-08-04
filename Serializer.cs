ff43468d3d6ade31602a3fac3eb8f8f60562eda5
dnewcome <djn125@yahoo.com>
2010-08-03 22:34:44 -0400

    first commit of serializer

---
+++ Serializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Djn.Framework
+{
+	/**
+	 * Helper class for dealing with .NET serialization
+	 * TODO: move to another project
+	 */
+	public class Serializer
+	{
+		public static void Serialize( Type in_type, Object in_instance, Stream out_stream ) {
+			System.Xml.Serialization.XmlSerializer xser = new System.Xml.Serialization.XmlSerializer( in_type );
+			xser.Serialize( out_stream, in_instance );
+		}
+
+		public static object Deserialize( Type in_type, Stream in_stream ) {
+			System.Xml.Serialization.XmlSerializer xser = new System.Xml.Serialization.XmlSerializer( in_type );
+			return xser.Deserialize( in_stream );
+		}
+	}
+}
