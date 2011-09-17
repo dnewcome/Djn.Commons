31e0289c24133ffb4ce5a54115f67d649c32d4e6
dnewcome <djn125@yahoo.com>
2011-09-16 23:30:55 -0700

    adding files to be intergrated into the library

---
+++ TODO/Util.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+* This file is from the spreedly project.
+* I had to do a special addition to prevent the xml
+* namespace declarations from appearing in the output.
+*/
+namespace Spreedly
+{
+	public class Util
+	{
+		public static T Deserialize<T>( Stream stream ) {
+			XmlSerializer serializer = new XmlSerializer( typeof( T ) );
+			return ( T )serializer.Deserialize( stream );
+		}
+		public static T DeserializeFromString<T>( string in_str ) {
+			MemoryStream ms = new MemoryStream( Encoding.UTF8.GetBytes( in_str ) );
+			ms.Seek( 0, SeekOrigin.Begin );
+			return Deserialize<T>( ms );
+		}
+
+		public static void Serialize<T>( T obj, Stream stream ) {
+			XmlSerializer serializer = new XmlSerializer( typeof( T ) );
+			serializer.Serialize(
+				stream,
+				obj,
+				new XmlSerializerNamespaces(
+					new XmlQualifiedName[] { 
+					new XmlQualifiedName( "", "" ) 
+				}
+				)
+			 );
+		}
+
+		/**
+		 * Convenience method for testing. Wrap serializer
+		 * in memorystream, returning string
+		 */
+		public static string SerializeToString<T>( T obj ) {
+			MemoryStream ms = new MemoryStream();
+			
+			Serialize<T>( obj, ms );
+			StreamReader sr = new StreamReader( ms );
+			ms.Seek( 0, SeekOrigin.Begin );
+			return sr.ReadToEnd();
+		}
+	}
+}
---
+++ TODO/util-crmshell.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Crm.Sdk;
+using System.Xml;
+using System.Text;
+using System.IO;
+using Microsoft.Xrm.Sdk;
+
+/**
+* This utility file came from the crmshell project. Need
+* to get the Contract serializers into the djn project.
+*/
+namespace CrmShell
+{
+	public class Util
+	{
+		public Util() { }
+
+		/**
+		 * Serialization method that uses DataContractSerializer - we can't use ordinary 
+		 * methods with CRM 2011 entities anymore.
+		 */
+		public static void SerializeToStream<T>( T in_obj, Stream in_stream ) {
+			System.Runtime.Serialization.DataContractSerializer serializer =
+				new System.Runtime.Serialization.DataContractSerializer( typeof( T ), null, int.MaxValue, false, false, null, new KnownTypesResolver() );
+			XmlTextWriter writer = new XmlTextWriter( in_stream, Encoding.UTF8 ) {
+				Formatting = Formatting.Indented
+			};
+			serializer.WriteObject( writer, in_obj );
+			writer.Close();
+		}
+
+		public static T DeSerializeFromStream<T>( Stream in_stream ) {
+			System.Runtime.Serialization.DataContractSerializer serializer =
+				new System.Runtime.Serialization.DataContractSerializer( typeof( T ), null, int.MaxValue, false, false, null, new KnownTypesResolver() );
+			return ( T )serializer.ReadObject( in_stream );
+		}
+		public static T DeSerializeFromDisk<T>( string in_filename ) {
+			FileStream fs = new FileStream( in_filename, FileMode.Open, FileAccess.Read );
+			return DeSerializeFromStream<T>( fs );
+		}
+
+		/**
+		* Format an entity collection for tabular console output
+		*/
+		public static void PrintTabular( EntityCollection entitycollection ) {
+			foreach( KeyValuePair<string, object> item in entitycollection.Entities.First().Attributes ) {
+				Console.Write( "{0,10}", item.Key );
+			}
+			Console.WriteLine();
+			foreach( Entity entity in entitycollection.Entities ) {
+				PrintTabular( entity );
+			}
+		}
+		public static void PrintTabular( Entity entity ) {
+			foreach( KeyValuePair<string, object> item in entity.Attributes ) {
+				Console.Write( "{0,10}", item.Value );
+			}
+			Console.WriteLine();
+		}
+
+		/**
+		* Format an entity collection for console output as property list
+		*/
+		public static void PrintList( EntityCollection entitycollection ) {
+			foreach( Entity entity in entitycollection.Entities ) {
+				PrintList( entity );
+			}
+		}
+		public static void PrintList( Entity entity ) {
+			foreach( KeyValuePair<string, object> item in entity.Attributes ) {
+				Console.WriteLine( "{0,15}: {1}", item.Key, item.Value );
+			}
+			Console.WriteLine();
+		}
+
+	}
+}
